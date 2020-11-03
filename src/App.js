1ee6564c3229de61d773a67a5b9eafd89e89ab89
Kesha Hall <k_renee10@yahoo.com>
2020-11-03 18:59:30 -0500

    added initial files and exact path

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Home from "./containers/Home/Home";
+import Portfolio from "./containers/Portfolio/Portfolio";
+import Contact from "./containers/Contact/Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        <Link to="/portfolio">Portfolio</Link>
+      </div>
+      <div>
+        <Link to="/contact">Contact</Link>
+      </div>
+      <Switch>
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Route path="/" component={Home} />
+      </Switch>
+      
+      </Router>
     </div>
   );
 }
